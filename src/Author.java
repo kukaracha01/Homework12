c208d38b6e2c62948e28ce07ab92c5a461c61216
mu <semkadarov@gmail.com>
2024-04-30 21:58:56 +0300

    Тут все подготовлено

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    String firstNameAuthor;
+    String secondNameAuthor;
+
+    public Author(String firstNameAuthor, String secondNameAuthor) {
+        this.firstNameAuthor = firstNameAuthor;
+        this.secondNameAuthor = secondNameAuthor;
+    }
+
+    public String getFirstNameAuthor() {
+        return this.firstNameAuthor;
+    }
+
+    public String getSecondNameAuthor() {
+        return this.secondNameAuthor;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstBook = "Капитанская дочка";
+        String secondBook = "Отцы и дети";
+        String firstAuthor = "А.Пушкин";
+        String secondAuthor = "И.Тургенев";
+        int yearFirstBook = 1836;
+        int yearSecondBook = 1860;
+        String firstNameFirstAuthor = "Александр";
+        String secondNameFirstAuthor = "Пушкин";
+        String firstNameSecondAuthor = "Иван";
+        String secondNameSecondAuthor = "Тургенев";
+        Book capitanDochka = new Book(firstBook, firstAuthor, yearFirstBook);
+        Author pushkinAleksandr = new Author(firstNameFirstAuthor, secondNameFirstAuthor);
+        Book fathersAndChildren = new Book(secondBook, secondAuthor, yearSecondBook);
+        Author turgenevIvan = new Author(firstNameSecondAuthor, secondNameSecondAuthor);
+
     }
 }
