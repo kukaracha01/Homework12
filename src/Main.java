5def90bf00577e9329c2683185a0eb795fec4d4f
mu <semkadarov@gmail.com>
2024-05-01 16:50:23 +0300

    Изменения 2

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-//        String firstBook = "Капитанская дочка";
-//        String secondBook = "Отцы и дети";
-//        String firstAuthor = "А.Пушкин";
-//        String secondAuthor = "И.Тургенев";
-//        int yearFirstBook = 1836;
-//        int yearSecondBook = 1860;
-//        String firstNameFirstAuthor = "Александр";
-//        String secondNameFirstAuthor = "Пушкин";
-//        String firstNameSecondAuthor = "Иван";
-//        String secondNameSecondAuthor = "Тургенев";
+        Author pushkinAleksandr = new Author("Александр Пушкин");
+        Author turgenevIvan = new Author("Иван Тургенев");
 
-        Author pushkinAleksandr = new Author("Александр", "Пушкин");
-        System.out.println(pushkinAleksandr.getFirstNameAuthor() + " " + pushkinAleksandr.getSecondNameAuthor());
-
-        Author turgenevIvan = new Author("Иван", "Тургенев");
-        System.out.println(turgenevIvan.getFirstNameAuthor() + " " + turgenevIvan.getSecondNameAuthor());
-
-        Book capitanDochka = new Book("Капитанская дочка", "А.Пушкин", 1836);
+        Book capitanDochka = new Book("Капитанская дочка", pushkinAleksandr.getNameAuthor(), 1836);
         System.out.println(capitanDochka.getTitleBook() + ", " + capitanDochka.getAuthorBook() + ", " + capitanDochka.getYearBook());
 
-        Book fathersAndChildren = new Book("Отцы и дети", "И.Тургенев", 1860);
+        Book fathersAndChildren = new Book("Отцы и дети", turgenevIvan.getNameAuthor(), 1860);
         System.out.println(fathersAndChildren.getTitleBook() + ", " + fathersAndChildren.getAuthorBook() + ", " + fathersAndChildren.getYearBook());
 
         fathersAndChildren.setYearBook(1861);
