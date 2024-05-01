19bafa9c9d763f4343037c1594a96ec97069563b
mu <semkadarov@gmail.com>
2024-05-01 19:22:02 +0300

    Изменения 3

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
         Author turgenevIvan = new Author("Иван Тургенев");
 
         Book capitanDochka = new Book("Капитанская дочка", pushkinAleksandr, 1836);
-        System.out.println(capitanDochka.getTitleBook() + ", " + capitanDochka.getAuthorBook() + ", " + capitanDochka.getYearBook());
+        System.out.println(capitanDochka.getTitleBook() + ", " + pushkinAleksandr.getNameAuthor() + ", " + capitanDochka.getYearBook());
 
         Book fathersAndChildren = new Book("Отцы и дети", turgenevIvan, 1860);
-        System.out.println(fathersAndChildren.getTitleBook() + ", " + fathersAndChildren.getAuthorBook() + ", " + fathersAndChildren.getYearBook());
+        System.out.println(fathersAndChildren.getTitleBook() + ", " + turgenevIvan.getNameAuthor() + ", " + fathersAndChildren.getYearBook());
 
         fathersAndChildren.setYearBook(1861);
         System.out.println(fathersAndChildren.getYearBook());
